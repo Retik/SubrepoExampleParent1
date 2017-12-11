63c3a5fbb73fed436ce7a568569a37a7757a9d60
Ian Holm <ian.g.holm@intel.com>
2017-12-11 14:27:36 -0800

    Initial commit

---
+++ SubrepoExample.Vendor1/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using SimpleInjector;
+using SimpleInjector.Packaging;
+using SubrepoExample.Common;
+
+namespace SubrepoExample.Vendor1
+{
+    public class Bootstrapper : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IVendor, Vendor1>();
+        }
+    }
+}
---
+++ SubrepoExample.Vendor1/Vendor1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SubrepoExample.Common;
+
+namespace SubrepoExample.Vendor1
+{
+    public class Vendor1 : IVendor
+    {
+        public string GetName()
+        {
+            return "Vendor1";
+        }
+    }
+}
